bb80fa46c86f517c294c8d92ecf93719b51d6a77
Aman Singh <60286652+feyn-aman@users.noreply.github.com>
2020-03-12 21:34:02 +0530

    Update Task_Part_1.py

--- Task_Part_1.py
+++ Task_Part_1.py
@@ -1,3 +1,11 @@
 from uuid import uuid4
 from django.db import models
-from helpers.misc import get_url_friendly
+
+class User(models.Model):
+    """A user of Book-ed"""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    roll_no = models.IntegerField(unique=True)
+    short_description = models.CharField(max_length=50, blank=True)
+    
